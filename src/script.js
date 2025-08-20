54ed1c3757ad1e982e42cf84e107d27a45a418ee
Shahveer Siddiqui <shahveer2122@gmail.com>
2025-08-20 20:35:30 +0500

    coded navbar and make it responsvie through js

---
+++ src/script.js
@@ -0,0 +1,27 @@
+let hamburger = document.getElementsByClassName("hamburger")[0]
+let mobnav = document.getElementById("mob-nav")
+let orderbutton = document.getElementById("mobile-order-btn")
+let orderdropdown = document.getElementById("mobile-order-dropdown")
+
+hamburger.addEventListener("click", () => {
+    mobnav.classList.toggle("-translate-x-full");
+});
+
+
+orderbutton.addEventListener("click", () => {
+    if(orderdropdown.style.maxHeight && orderdropdown.style.maxHeight !== "0px") {
+        orderdropdown.style.maxHeight = "0px"
+    } else {
+        orderdropdown.style.maxHeight = orderdropdown.scrollHeight + "px"
+    }
+
+    let arrow = orderbutton.querySelector("svg")
+    arrow.classList.toggle("rotate-180")
+})
+
+document.addEventListener("click", (e) => {
+  // Check if click is NOT inside sidebar or hamburger
+  if (!mobnav.contains(e.target) && !hamburger.contains(e.target)) {
+    mobnav.classList.add("-translate-x-full"); // Close sidebar
+  }
+});
