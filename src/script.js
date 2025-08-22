89e6f2a20e05998bf9879018a593cd750b51788f
Shahveer Siddiqui <shahveer2122@gmail.com>
2025-08-22 08:01:41 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,23 +1,23 @@
-let hamburger = document.getElementsByClassName("hamburger")[0]
-let mobnav = document.getElementById("mob-nav")
-let orderbutton = document.getElementById("mobile-order-btn")
-let orderdropdown = document.getElementById("mobile-order-dropdown")
+// Hamburger toggle
+let hamburger = document.getElementsByClassName("hamburger")[0];
+let mobnav = document.getElementById("mob-nav");
+let orderbutton = document.getElementById("mobile-order-btn");
+let orderdropdown = document.getElementById("mobile-order-dropdown");
 
 hamburger.addEventListener("click", () => {
     mobnav.classList.toggle("-translate-x-full");
 });
 
-
 orderbutton.addEventListener("click", () => {
     if(orderdropdown.style.maxHeight && orderdropdown.style.maxHeight !== "0px") {
-        orderdropdown.style.maxHeight = "0px"
+        orderdropdown.style.maxHeight = "0px";
     } else {
-        orderdropdown.style.maxHeight = orderdropdown.scrollHeight + "px"
+        orderdropdown.style.maxHeight = orderdropdown.scrollHeight + "px";
     }
 
-    let arrow = orderbutton.querySelector("svg")
-    arrow.classList.toggle("rotate-180")
-})
+    let arrow = orderbutton.querySelector("svg");
+    arrow.classList.toggle("rotate-180");
+});
 
 document.addEventListener("click", (e) => {
   if (!mobnav.contains(e.target) && !hamburger.contains(e.target)) {
